7278b4cd12f5900c808568c150a51eaa679bf856
bochar68 <bochar.dg@gmail.com>
2024-09-19 16:26:45 +0300

    Заведен модуль Методы строк

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# Методы строк
+my_string = 'Текст для проверки работы методов'
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
