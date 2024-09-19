75b495d2d2b819e4e0e385cadc66bf5fd55c7c2d
bochar68 <bochar.dg@gmail.com>
2024-09-19 14:58:57 +0300

    Заведен модуль Динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = 'Дмитрий'
+print (f'Name: {name}')
+age = 56
+print (f'Age: {age}')
+age += 2
+print (f'New age: {age}')
+is_student = True
+print (f'Is Student: {is_student}')
