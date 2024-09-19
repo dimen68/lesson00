7eb1f5d326dbc8f17e881941eded8ce702a1fb39
bochar68 <bochar.dg@gmail.com>
2024-09-19 16:47:08 +0300

    Добавлены обозначения выводимых результатов

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,8 @@
 # Методы строк
-my_string = 'Текст для проверки работы методов'
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
+my_string = input('Введите текст для проверки работы методов: ')
+print('Количество символов введённого текста:',len(my_string))
+print('Строка в верхнем регистре:',my_string.upper())
+print('Строка в нижнем регистре:',my_string.lower())
+print('Строка без пробелов:',my_string.replace(' ', ''))
+print('Первый символ строки:',my_string[0])
+print('Последний символ строки:',my_string[-1])
