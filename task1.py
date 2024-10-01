594ff2a58f4fcd54ad9dafeb8bf6f9ad7d6fa5de
bochar68 <bochar.dg@gmail.com>
2024-10-01 19:03:59 +0300

    Из элементов словаря соберите дату в формате

---
+++ task1.py
@@ -0,0 +1,8 @@
+# Из элементов этого словаря соберите дату
+birdsday = {
+	'year' : '2025',
+	'month': '12',
+	'day'  : '31',
+}
+birdsday_data = birdsday['year'] + '-' + birdsday['month'] + '-' + birdsday['day']
+print(birdsday_data)
