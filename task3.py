11bbbb785439e528a03b0e9033328298666624ef
bochar68 <bochar.dg@gmail.com>
2024-10-01 19:49:58 +0300

    Дополнительные задачик Python

---
+++ task3.py
@@ -0,0 +1,12 @@
+'''# Получите из списка два последних элемента
+v = [1, 2, 3, 4, 5, 6]
+print(v[4::])
+'''
+# Получите сет  символов
+w = 'abcdeabc'
+kit_j = list(w)
+print(kit_j)
+kit_j.sort()
+print(kit_j)
+kit = set(kit_j)
+print(kit)
